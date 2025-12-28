9b4c041817fff87669133c49fccc1f2ba2066eda
InsightLoop Creator <passiongrow88@gmail.com>
2025-12-28 20:35:33 +0800

    fix tailwind + enable gemini api

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+export default {
+  content: [
+    "./index.html",
+    "./index.tsx",
+    "./App.tsx",
+    "./components/**/*.{ts,tsx}",
+    "./services/**/*.{ts,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
